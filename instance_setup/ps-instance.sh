b11167abdef99343440956edb73c7f8fd2f558a6
Juan Lee <me@juany.kr>
2025-09-21 08:14:42 +0900

    feat: Removed awscli v2 in ps instance

--- instance_setup/ps-instance.sh
+++ instance_setup/ps-instance.sh
@@ -21,12 +21,6 @@ sudo systemctl enable docker
 sudo systemctl start docker
 sudo usermod -aG docker $USER
 
-echo "Installing AWS CLI..."
-curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-unzip awscliv2.zip
-sudo ./aws/install
-rm -rf aws awscliv2.zip
-
 echo "Installing kubectl..."
 VERSION=$(curl -L -s https://dl.k8s.io/release/stable.txt)
 curl -LO "https://dl.k8s.io/release/$VERSION/bin/linux/amd64/kubectl"
